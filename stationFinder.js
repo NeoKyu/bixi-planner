a32a1d8b3a250644419a021f51c6523f6f276422
Michael Foo <foomic.shor@gmail.com>
2017-05-28 18:34:01 -0400

    functions to find closest stations and calculate travel times and distance are now complete.

--- stationFinder.js
+++ stationFinder.js
@@ -1,60 +1,80 @@
+// TODO:
+// - "cache" travel times
+// - cleanup code:
+//      - better names for variables in getTravelTimes
+//      - better column names in the db
+
 "use strict";
 
 var fs = require("fs");
 var request = require("request");
-
 var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
+var assert = require('assert');
+
+var current_location = [ -73.57685, 45.498964 ];
 var url_mongo = 'mongodb://localhost:27017/test';
+var api_key = fs.readFileSync('../directionsapikey.txt').toString();
 
-var location = [-73.576856,45.498964];
+var findClosestStations = function(location, db) {
 
-var findStations = function(location, db, callback) {
-   var cursor =db.stations.find({
-                                "ba": { $ne: 0},
-                                "loc": {
-                                    $near: {
-                                        $geometry: {
-                                            type: "Point" ,
-                                            coordinates: [ -73.57685, 45.498964 ]
-                                        },
-                                        $maxDistance: 1000,
+    // Find 5 closest non-empty stations within 1km
+    db.collection('stations').find({
+                                    "ba": { $ne: 0 },
+                                    "loc": {
+                                        $near: {
+                                            $geometry: {
+                                                type: "Point" ,
+                                                coordinates: location
+                                            },
+                                            $maxDistance: 1000,
+                                            }
                                         }
-                                    }
-                                }).limit(10);
-   cursor.each(function(err, doc) {
-      assert.equal(err, null);
-      if (doc != null) {
-         console.dir(doc);
-      } else {
-         callback();
-      }
-   });
+                                    }).limit(5).toArray(function(err, stations_list) {
+                                        if(err) {
+                                            console.log('Error:', err);
+                                            return [];
+                                        }
+                                        else  {
+                                            db.close();
+                                            getTravelTimes(stations_list);
+                                        }
+                                    });
 };
 
-var getJSON = function(api_key) {
-    var url_JSON = "https://maps.googleapis.com/maps/api/directions/json?origin=45.498964,-73.576856&destination=45.49947,-73.57591&departure_time=now&mode=walking&key=" + api_key;
-    request.get({
-        url: url_JSON,
-        json: true,
-    }, (err, res, data) => {
-        if (err) {
-        console.log('Error:', err);
-        } else if (res.statusCode !== 200) {
-        console.log('Status:', res.statusCode);
+var getTravelTimes = function(locations) {
+    var travel_times = [];
+    locations.forEach(function(location) {
+        var url_JSON = "https://maps.googleapis.com/maps/api/directions/json?origin="
+                        +current_location[1]+","
+                        +current_location[0]+"&destination="
+                        +location["loc"][1]+","
+                        +location["loc"][0]+"&departure_time=now&mode=walking&key="
+                        +api_key;
+        request.get({
+            url: url_JSON,
+            json: true,
+        }, (err, res, data) => {
+            if (err) {
+            console.log('Error:', err);
+            } else if (res.statusCode !== 200) {
+            console.log('Status:', res.statusCode);
         } else {
-        console.log(data["routes"][0]["legs"][0]["duration"]["value"]);
-        }
+            console.log(data["routes"][0]["legs"][0]["distance"]["value"] + " meters");
+            console.log(data["routes"][0]["legs"][0]["duration"]["value"] + " seconds");
+
+            //replace console.log with actual function or TODO here
+            console.log( {
+                "loc":location,
+                "time":data["routes"][0]["legs"][0]["duration"]["value"],
+                "distance":data["routes"][0]["legs"][0]["distance"]["value"]
+                });
+            }
+        });
     });
 }
 
-var api_key = fs.readFileSync('../directionsapikey.txt').toString();
-getJSON(api_key);
-
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  findStations(location, db, function() {
-      db.close();
-  });
+MongoClient.connect(url_mongo, function(err, db) {
+    assert.equal(null, err);
+    findClosestStations(current_location, db);
 });
